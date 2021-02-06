ffb1ed2eb6e330458907c8f2617bb4f479cef7d7
Aaron Brennan <aaron.brennan@bryanuniversity.edu>
2021-02-06 09:54:07 -0800

    your message

---
+++ index2.js
@@ -0,0 +1,5 @@
+var request = new XMLHttpRequest()
+ 
+request.open('GET', 'https://api.vschool.io/[aaronbrennan]/todo/', true)
+
+// request.onload .....? theres the API above. Im not sure if you are able to access it though.
